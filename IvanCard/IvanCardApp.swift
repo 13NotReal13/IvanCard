4c8e17717750f8c0cb39295d5561830242fb0445
Ivan Semikin <i.semikin.94@icloud.com>
2024-07-23 10:15:09 +0200

    Initial Commit

---
+++ IvanCard/IvanCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  IvanCardApp.swift
+//  IvanCard
+//
+//  Created by Иван Семикин on 23/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct IvanCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
